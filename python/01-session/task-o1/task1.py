b521e4c8cac9c9b0936b6ecfbd01fb5ed45c827f
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-03-17 18:41:00 +0530

    task 01

---
+++ python/01-session/task-o1/task1.py
@@ -0,0 +1,7 @@
+fName=input("Enter Your first name : ")
+mName=input("Enter Your middle name : ")
+lName=input("Enter Your last name : ")
+age=input("Enter Your age : ")
+
+
+print("your name is : "+fName+" "+mName+" "+lName+". "+"age is : "+age)
