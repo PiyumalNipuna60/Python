57b321cc354abe613bfed02092a3162b3ae40557
nipuna_piyumal <nipuna315np@gmail.com>
2023-03-21 22:53:01 +0530

    Complete task 2

---
+++ python/01-session/task-o1/task2.py
@@ -0,0 +1,11 @@
+menu=["chicken","bun","rice","kottu"]
+
+print(menu)
+
+food=input("What is you wont ? ")
+
+if (food=="chicken" or food=="bun" or food=="rice" or food=="kottu") :
+    print("Yse "+food+" available !")
+
+else:
+    print("Not available !")
