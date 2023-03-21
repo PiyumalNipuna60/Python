d97858dd15e2bd3354f293c6f3ff64d73d782188
nipuna_piyumal <nipuna315np@gmail.com>
2023-03-21 22:38:05 +0530

    Ad

---
+++ python/01-session/04-Conditional/Conditional.py
@@ -0,0 +1,48 @@
+x=1
+
+print(x==1)  #samanada kiyala check karanava
+
+print(x!=1) #asamanada kiyala check karanava
+
+print(x<=1) # kuda or samanada kiyala check karanava
+
+print(x>=1) # vishala or samanada kiyala check karanava
+
+
+
+#=================================================
+
+name="piyumal"
+
+if name=="piyumal":
+        print("Hy Piyumal")
+else :
+    print("wrong name")
+
+
+name="Nipuna"
+age=20
+
+
+#=================================================
+#   and operation
+
+if name=="Nipuna" and age==17 :  # and operation
+        print("Hy Piyumal")
+else :
+    print("wrong name")
+
+
+    print("--------------")
+
+
+#=================================================
+#   or operation
+
+    if (name=="Nipuna" or age==17 ) : # or operation
+        print("Hy Piyumal")
+        print("wrong")
+    else:
+        print("wrong")
+
+            
