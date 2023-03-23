bdefddd2a12534114fa419b2039184a8db97e6c1
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-03-23 12:57:25 +0530

    Introduct basic-oparation

---
+++ python/02-session/01-basic-oparation/Basic-oparation.py
@@ -0,0 +1,34 @@
+
+#addition
+print(8+2)   # 8+2=10
+
+#substraction
+print(8-2)   # 8-2=6
+
+#multiplication
+print(8*2)   # 8*2=16
+
+#flow multiplication / exponent
+print(8**2)  # 8*2=64
+
+#devition
+print(8/2)   # 8/2=4.0
+
+#flow devition
+print(8//2)   # 8/2=4
+
+#modulus
+print(8%2)   # 8%2=0
+
+
+
+
+x=[1,3,5]
+print(x*2)  #[1, 3, 5, 1, 3, 5]
+
+# y=[2,4,6]
+# print(x*y) # Error
+
+m=["1","2","3"]
+print(m*2) #['1', '2', '3', '1', '2', '3']
+
