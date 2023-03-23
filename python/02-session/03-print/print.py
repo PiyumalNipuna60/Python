3cd4f89a04fef9b5c6fbfd97e71a3413cf57407c
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-03-23 15:28:40 +0530

    Introduct print

---
+++ python/02-session/03-print/print.py
@@ -0,0 +1,15 @@
+name="kamal"
+age=20
+
+print("kamal")
+print(name)
+
+print("Name is : "+name)
+
+print("Name is : "+name+ ". His age is : "+str(age))
+
+print("Name is : %s and His age is : %d" %(name,age))
+
+x=10.5254
+print("Prise is %f" % x)
+print("Prise is %.2f" %x)
