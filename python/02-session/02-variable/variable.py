47465e06897310ee748c20a9066e4c8fb6a5e8bc
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-03-23 15:16:07 +0530

    Introduct variables

---
+++ python/02-session/02-variable/variable.py
@@ -0,0 +1,19 @@
+a=10
+print(type(a) ) #int
+
+b=10.2
+print(type(b)) #float
+
+c="K"
+d="nipuna"
+print(type(c)) #string
+print(type(d)) #string
+
+e=3+4j
+print(type(e)) #complex
+
+f=False
+print(type(f))  #bool
+
+
+
